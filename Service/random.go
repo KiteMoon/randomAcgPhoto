ff8cfcebf41f4fbd7a262f046efcf22a087141aa
KiteMoon <xie@loli.fit>
2022-08-11 22:24:30 +0800

    ---该版本有问题--- 已知问题:该版本会因为随机ID生成机制的原因导致出现无法匹配的问题 （PID是基于全表生成的，但是有的时候随机到的PID对应的图片并不是崩三图，就会出现错误）

--- Service/random.go
+++ Service/random.go
@@ -3,6 +3,7 @@ package Service
 import (
 	"database/sql"
 	"fmt"
+	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/valyala/fastrand"
 )
@@ -28,4 +29,43 @@ func RandomImg(DB *sql.DB, tableLen int) (string, error) {
 	}
 	return sqlReutrnData.CdnUrl + sqlReutrnData.Path, nil
 	//返回
+}
+
+// QueryRandomImg 精确筛选
+func QueryRandomImg(context *gin.Context, DB *sql.DB, tableLen int) (string, error) {
+	workName := context.Query("imgBClass")    //ACG类下游戏或者作品
+	officalType := context.Query("imgCClass") // 官方/三方
+	// 如果用户没有匹配任何作品
+	if workName == "" {
+		return RandomImg(DB, tableLen)
+	}
+	// 查询为官方作品
+	if officalType == "offical" {
+		sqlReutrnData := new(randomImgReturnSql)
+		sqlQueryErr := DB.QueryRow("SELECT TITLE,URL,CDN_URL FROM RANDOM_PHOTO_TABLE WHERE PID=? AND OFFICAL=1 AND WORK=?", fastrand.Uint32n(uint32(tableLen)), workName).Scan(&sqlReutrnData.Title, &sqlReutrnData.Path, &sqlReutrnData.CdnUrl)
+		if sqlQueryErr != nil {
+			fmt.Println("查询失败")
+			fmt.Println(sqlQueryErr)
+			return "查询失败\n错误码:RandomImg_001", sqlQueryErr
+		}
+		return sqlReutrnData.CdnUrl + sqlReutrnData.Path, nil
+	} else if officalType == "parties" { //识别为三方
+		sqlReutrnData := new(randomImgReturnSql)
+		sqlQueryErr := DB.QueryRow("SELECT TITLE,URL,CDN_URL FROM RANDOM_PHOTO_TABLE WHERE PID=? AND OFFICAL=0 AND WORK=?", fastrand.Uint32n(uint32(tableLen)), workName).Scan(&sqlReutrnData.Title, &sqlReutrnData.Path, &sqlReutrnData.CdnUrl)
+		if sqlQueryErr != nil {
+			fmt.Println("查询失败")
+			fmt.Println(sqlQueryErr)
+			return "查询失败\n错误码:RandomImg_001", sqlQueryErr
+		}
+		return sqlReutrnData.CdnUrl + sqlReutrnData.Path, nil
+	} else {
+		sqlReutrnData := new(randomImgReturnSql)
+		sqlQueryErr := DB.QueryRow("SELECT TITLE,URL,CDN_URL FROM RANDOM_PHOTO_TABLE WHERE PID=? AND WORK=?", fastrand.Uint32n(uint32(tableLen)), workName).Scan(&sqlReutrnData.Title, &sqlReutrnData.Path, &sqlReutrnData.CdnUrl)
+		if sqlQueryErr != nil {
+			fmt.Println("查询失败")
+			fmt.Println(sqlQueryErr)
+			return "查询失败\n错误码:RandomImg_001", sqlQueryErr
+		}
+		return sqlReutrnData.CdnUrl + sqlReutrnData.Path, nil
+	}
 }
